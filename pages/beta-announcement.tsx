d950bb06b7a74da57d53d536a35de08ad653e065
mojokojoxd <kwadwoneer@gmail.com>
2023-01-28 00:05:12 -0700

    get started to join now on beta-announcement

--- pages/beta-announcement.tsx
+++ pages/beta-announcement.tsx
@@ -45,7 +45,7 @@ export default function BetaAnnouncement() {
                 href='#'
                 className='rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
               >
-                Get started
+                Join now
               </a>
               <a
                 href='#'
