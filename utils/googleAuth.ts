92108068aff778480b95b349544914ec6cfaebd4
mojokojoxd <kwadwoneer@gmail.com>
2023-03-22 13:35:34 -0600

    testing google auth

--- utils/googleAuth.ts
+++ utils/googleAuth.ts
@@ -6,5 +6,5 @@ import type { OAuth2Client } from "google-auth-library";
 export const oauth2Client = new google.auth.OAuth2(
     keys.web.client_id,
     keys.web.client_secret,
-    keys.web.redirect_uris[0]
+    keys.web.redirect_uris[1]
 )
