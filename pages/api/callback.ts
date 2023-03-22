7b7ed7bd51e842bcbb022730f345897a61898bd6
mojokojoxd <kwadwoneer@gmail.com>
2023-03-22 15:23:07 -0600

    testing google auth

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -3,15 +3,17 @@ import { stashTokens } from "../../utils/mongoConfig";
 import { oauth2Client } from "../../utils/googleAuth";
 import url from 'url'
 
+
 const callBack:NextApiHandler = async(req,res) => {
 
     if ((req.url as string).indexOf('/callback') > -1) {
-        const qs = new url.URL(req.url as string, 'https://upforce-africa-vercel.app')
+        const qs = new url.URL(req.url as string, 'https://upforce-africa.vercel.app')
           .searchParams;
         res.end('Authentication successful! Please return to the console.');
         //@ts-ignore
         const {tokens}:{tokens: Credentials } = await oauth2Client.getToken(qs.get('code'));
         await stashTokens(tokens);
+        oauth2Client.credentials = tokens; // eslint-disable-line require-atomic-updates
       }else {
         res.status(400).json({message: "authorization failed"})
       }
