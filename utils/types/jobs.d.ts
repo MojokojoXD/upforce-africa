f83d9a592e072c3d79d0ac6cdc678409b072e187
mojokojoxd <kwadwoneer@gmail.com>
2023-04-10 18:14:50 -0700

    testing using object definition for question ids

--- utils/types/jobs.d.ts
+++ utils/types/jobs.d.ts
@@ -3,3 +3,8 @@ import type { forms_v1 } from "googleapis"
 export interface ApprovedJobs extends forms_v1.Schema$FormResponse{
     approvedAt?: string | null | undefined;
 }
+
+export type GoogleQuestionIds = {
+    name:string;
+    id:string;
+}
--- utils/vars.ts
+++ utils/vars.ts
@@ -1,9 +1,60 @@
+import type { GoogleQuestionIds } from './types/jobs';
+
 export const content_links = [
-    {name: 'sales',href: 'docs.google.com/spreadsheets/d/1m_FiOsDPg9XqA2lKqLFZMTsgamv8Cc7NU927d3HMkIw/edit#gid=0'},
-    {name: 'engineering',href:'docs.google.com/spreadsheets/d/1P-ZTaFHPdSc-dED6tYGtiO9UNfY4sv9ZmQ2iORKtRsc/edit#gid=0'},
-    {name: 'product',href:'docs.google.com/spreadsheets/d/1L2dYTbCHF_EJwsUtj7BaZgEtRfCql96y4e6Z1KdQx7U/edit#gid=0'},
-    {name: 'marketing',href:'docs.google.com/spreadsheets/d/1w8UobmXsg0cV2xA44m4eu_569EvNrPJbYDX2cYJ6ldQ/edit#gid=0'},
-    {name: 'design',href:'docs.google.com/spreadsheets/d/19gaIrG0JLL2OwflMVMDhsCbdqN2YbOmytJaCNkWstsk/edit#gid=0'},
-    {name: 'data science',href:'docs.google.com/spreadsheets/d/1IeVIJjP0zA1LXUSHrPCxGsd_ElkAxossGPWx-1Ezb8g/edit#gid=0'},
-    // {name: 'finance', href: ''},
-]
+  {
+    name: 'sales',
+    href: 'docs.google.com/spreadsheets/d/1m_FiOsDPg9XqA2lKqLFZMTsgamv8Cc7NU927d3HMkIw/edit#gid=0',
+  },
+  {
+    name: 'engineering',
+    href: 'docs.google.com/spreadsheets/d/1P-ZTaFHPdSc-dED6tYGtiO9UNfY4sv9ZmQ2iORKtRsc/edit#gid=0',
+  },
+  {
+    name: 'product',
+    href: 'docs.google.com/spreadsheets/d/1L2dYTbCHF_EJwsUtj7BaZgEtRfCql96y4e6Z1KdQx7U/edit#gid=0',
+  },
+  {
+    name: 'marketing',
+    href: 'docs.google.com/spreadsheets/d/1w8UobmXsg0cV2xA44m4eu_569EvNrPJbYDX2cYJ6ldQ/edit#gid=0',
+  },
+  {
+    name: 'design',
+    href: 'docs.google.com/spreadsheets/d/19gaIrG0JLL2OwflMVMDhsCbdqN2YbOmytJaCNkWstsk/edit#gid=0',
+  },
+  {
+    name: 'data science',
+    href: 'docs.google.com/spreadsheets/d/1IeVIJjP0zA1LXUSHrPCxGsd_ElkAxossGPWx-1Ezb8g/edit#gid=0',
+  },
+  // {name: 'finance', href: ''},
+];
+
+export const googleQuestionIds: GoogleQuestionIds[] = [
+  {
+    name: 'jobTitle',
+    id: '1cf59252',
+  },
+  {
+    name: 'company',
+    id: '1c017378',
+  },
+  {
+    name: 'duties',
+    id: '2d0bb8c6',
+  },
+  {
+    name: 'requirements',
+    id: '25b9bb0e',
+  },
+  {
+    name: 'compensation',
+    id: '484da9c7',
+  },
+  {
+    name: 'location',
+    id: '24b2a3dd',
+  },
+  {
+    name: 'applicationUrl',
+    id: '1dfa0c26',
+  }
+];
