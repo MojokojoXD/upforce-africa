b498965bd01384b83988cd7817fc9d8df0bedf64
mojokojoxd <kwadwoneer@gmail.com>
2023-04-21 11:33:38 -0600

    added a way to post jobs directly from admin area

--- components/admin/panelOptions/DirectPost.tsx
+++ components/admin/panelOptions/DirectPost.tsx
@@ -202,8 +202,8 @@ const DirectPost: FC<DirectPostProps> = () => {
                     <textarea
                       name='duties'
                       id='duties'
-                      cols='30'
-                      rows='10'
+                      cols={30}
+                      rows={10}
                       className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                       defaultValue={''}
                     />
@@ -223,8 +223,8 @@ const DirectPost: FC<DirectPostProps> = () => {
                     <textarea
                       name='qualifications'
                       id='qualifications'
-                      cols='30'
-                      rows='10'
+                      cols={30}
+                      rows={10}
                       className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                       defaultValue={''}
                     />
