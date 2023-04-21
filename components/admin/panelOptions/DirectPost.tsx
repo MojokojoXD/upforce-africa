e25fe8d8426765eb51a4836e7d8ee8ab3471f121
mojokojoxd <kwadwoneer@gmail.com>
2023-04-21 11:37:15 -0600

    added a way to post jobs directly from admin area

--- components/admin/panelOptions/DirectPost.tsx
+++ components/admin/panelOptions/DirectPost.tsx
@@ -204,7 +204,7 @@ const DirectPost: FC<DirectPostProps> = () => {
                       id='duties'
                       cols={30}
                       rows={10}
-                      className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                      className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-3'
                       defaultValue={''}
                     />
                   </div>
@@ -225,7 +225,7 @@ const DirectPost: FC<DirectPostProps> = () => {
                       id='qualifications'
                       cols={30}
                       rows={10}
-                      className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                      className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-3'
                       defaultValue={''}
                     />
                   </div>
