54f8d923d7d48337b891968cf0d102575aad4db6
mojokojoxd <kwadwoneer@gmail.com>
2023-05-24 20:26:37 -0600

    added reveal effects

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -26,7 +26,7 @@ const Header: FC<HeaderProps> = ({ title, children }) => {
         <div className='h-full flex relative'>
           <div className='absolute w-full top-20 bottom-0'>
             <div className='mx-auto max-w-xs sm:max-w-7xl px-2 sm:px-6 lg:px-8 h-full flex justify-center flex-col overflow-hidden'>
-            <Fade triggerOnce direction='up' delay={500}>
+            <Fade triggerOnce direction='up' delay={1000} duration={500}>
                 <div className='space-y-8'>
                     <h1 className='text-left text-4xl sm:text-5xl font-light w-full sm:max-w-lg tracking-tight'>
                         Building Africa’s Largest Network of Professional Experts
